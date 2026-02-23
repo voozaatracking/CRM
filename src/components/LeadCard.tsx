382f59c5749dd0c19e30ed707f700e8cd3b9616e
voozaatracking <imvest-managment@outlook.de>
2026-02-23 17:53:30 +0100

    Update LeadCard.tsx

--- src/components/LeadCard.tsx
+++ src/components/LeadCard.tsx
@@ -36,9 +36,7 @@ export default function LeadCard({ lead }: { lead: Lead }) {
             className="text-gray-400 hover:text-blue-600 shrink-0"
             title="Link öffnen"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101M10.172 13.828a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
-            </svg>
+            🔗
           </a>
         )}
       </div>
