3d820d0eeefc1775eea5bdfd1af1a42f7d6900c6
voozaatracking <imvest-managment@outlook.de>
2026-03-05 19:45:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,16 @@ const nextConfig = {
   experimental: {
     serverActions: { bodySizeLimit: '5mb' },
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *" },
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
