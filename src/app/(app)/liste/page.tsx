51e923cc6644066cd532895976e18799e5a71c45
voozaatracking <imvest-managment@outlook.de>
2026-03-16 15:45:31 +0100

    Update page.tsx

--- src/app/(app)/liste/page.tsx
+++ src/app/(app)/liste/page.tsx
@@ -2,6 +2,8 @@ import { getLeads } from "@/actions/leads";
 import LeadList from "@/components/LeadList";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export default async function ListPage() {
   const leads = await getLeads();
